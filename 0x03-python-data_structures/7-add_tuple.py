46b4a3a91f699add6a1bd6513eb8428210b652d7
moanbekele <moanbekele@gmail.com>
2023-01-09 02:21:18 -0800

    debuging

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,13 +9,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif lenA < 2:
         tuple_a = (tuple_a[0], 0)
 
-
 # check for tuple_b
     if lenB < 1:
         tuple_b = (0, 0)
     elif lenB < 2:
         tuple_b = (tuple_b[0], 0)
-
 
     res = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return res

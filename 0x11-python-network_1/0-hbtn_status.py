8e1aa5a7b73f58def20c48fe623517cd4362f5c4
moanbekele <moanbekele@gmail.com>
2023-05-02 01:26:11 -0700

    last

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://intranet.hbtn.io/status.
-- uses urlib package
+"""
+- Python script that fetches https://alx-intranet.hbtn.io/status
+- imports urlib package
 """
 
 
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
-        print("\t   - type: {}".format(type(content)))
-        print("\t   - content: {}".format(content))
-        print("\t   - utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
